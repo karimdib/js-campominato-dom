f9e6e5786f449ad300b2806af772a7b53b37648f
karimdib <karim.dib2001@gmail.com>
2023-10-16 17:29:08 +0200

    minefield DOM

---
+++ js/app.js
@@ -0,0 +1,65 @@
+const grigliaDOMElement = document.querySelector('.griglia')
+const buttonDOMElement = document.querySelector('.button')
+let pointGame = 0
+
+buttonDOMElement.addEventListener('click', function() {
+    grigliaDOMElement.innerHTML = ''
+    
+    grigliaDOMElement.classList.remove('gameover')
+
+    const bombsArray = getArrayOfRandomIntBetween(1, 100, 16)
+    console.log( bombsArray)
+
+    for(let i = 0; i < 100; i++){
+
+        const htmlItem = `<div class="griglia__items">${i + 1}</div>`
+    
+        grigliaDOMElement.innerHTML += htmlItem
+        
+    }
+
+    const itemsGriglia = document.querySelectorAll('.griglia__items')
+
+    for (let i = 0; i < itemsGriglia.length; i++){
+
+        const itemCorrente  = itemsGriglia[i]
+		
+        itemCorrente.addEventListener('click', function() {
+            console.log('Hai cliccato sulla casella numero:', i + 1)
+            if (bombsArray.includes(i + 1)) {
+                itemCorrente.classList.add('red')
+                grigliaDOMElement.classList.add('gameover')
+                alert('Hai perso! Clicca sul bottone per iniziare una nuova partita.')
+                const pointDOmelement = document.getElementById('point')
+                pointDOmelement.innerHTML = pointGame 
+            } else {
+                itemCorrente.classList.add('skyblue')
+                pointGame++
+            }
+        })
+        if(pointGame === 84){
+            console.log('hai vinto')
+        }
+    }
+})
+
+function getArrayOfRandomIntBetween(minRange, maxRange, number) {
+    const bombsArray = []
+
+    while (bombsArray.length < 16) {
+        const n = getRandomIntInclusive(1, 100)
+
+        if (!bombsArray.includes(n)) {
+            bombsArray.push(n)
+        }
+    }
+
+    return bombsArray
+}
+
+function getRandomIntInclusive(min, max) {
+	min = Math.ceil(min)
+	max = Math.floor(max)
+	return Math.floor(Math.random() * (max - min + 1) + min) 
+}
+
---
+++ readme.md
@@ -0,0 +1,11 @@
+# CAMPOMINATO DOM
+
+- creo un array 
+- Genero 16 numeri casuali
+  - Assicurarsi che non ci siano numeri uguali 
+- pusho i 16 numeri nell array 
+- Metto a confronto i numeri generati con le celle della griglia:
+  - SE il numero generato è stato cliccato dall'utente 
+    - GameOver, la cella si colora di rosso e il punteggio viene stampato
+  - ALTRIMENTI 
+   - continua a giocare, la cella si colora di azzurro
